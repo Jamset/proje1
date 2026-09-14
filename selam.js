dc308571f55ff73a34ba76baf6aeaca8e7124d80
Jamset <guserjanset@gmail.com>
2026-09-14 19:46:02 +0300

    JavaScript degiskenler calismasi

--- selam.js
+++ selam.js
@@ -1,1 +1,10 @@
 console.log("Merhaba dünya");
+
+let yas=25; // sonradan değiştirilebilir 
+const isim ="Arda"; // sonradan değiştirilmez
+
+let puan = 10;
+puan =20; // sorun yok let ile değiştirilebilir 
+const dogumyili=2001;
+dogumyili=2002; // HATA VERİR CONT DEĞİŞTİRİLMEZ 
+
